87e2ab13b4987f28bfc655dae2298561e39990db
jef-design <bermejojff97@gmail.com>
2021-08-14 18:42:09 +0800

    fix gnews api for search

--- pages/results/[search].js
+++ pages/results/[search].js
@@ -1,50 +1,50 @@
-// import React, { useEffect } from "react";
-// import { fetchSearch } from "../../store/actions/searchAction";
-// import { useDispatch, useSelector } from "react-redux";
-// import NewsCard from "../../components/Article/NewsCard";
-// import Head from 'next/head'
+import React, { useEffect } from "react";
+import { fetchSearch } from "../../store/actions/searchAction";
+import { useDispatch, useSelector } from "react-redux";
+import NewsCard from "../../components/Article/NewsCard";
+import Head from 'next/head'
 
-// function SearchResult() {
-//     const dispatch = useDispatch();
-//     const query = useSelector(state => state.search.query)
-//     const search = useSelector(state => state.search.searchnews);
+function SearchResult() {
+    const dispatch = useDispatch();
+    const query = useSelector(state => state.search.query)
+    const search = useSelector(state => state.search.searchnews);
 
 
-//     useEffect(() => {
-//         dispatch(fetchSearch(query));
-//     }, [dispatch,query]);
-//     return (
-//         <section>
-//             <Head>
-//                 <title>Google News - Search - {query}</title>
-//             </Head>
+    useEffect(() => {
+        dispatch(fetchSearch(query));
+    }, [dispatch,query]);
+    return (
+        <section>
+            <Head>
+                <title>Google News - Search - {query}</title>
+            </Head>
 
-//             {search &&
-//                 search.map((b, i) => {
-//                     const {
-//                         title,
-//                         urlToImage,
-//                         description,
-//                         publishedAt,
-//                         source = [],
-//                         url,
-//                         content,
-//                     } = b;
-//                     return (
-//                         <NewsCard
-//                             key={i}
-//                             title={title}
-//                             urlToImage={urlToImage}
-//                             description={description}
-//                             url={url}
-//                             publishedAt={publishedAt}
-//                             source={source}
-//                             content={content}
-//                         />
-//                     );
-//                 })}
-//         </section>
-//     )
-// }
+            {search &&
+                search.map((b, i) => {
+                    const {
+                        title,
+                        urlToImage,
+                        description,
+                        publishedAt,
+                        source = [],
+                        url,
+                        content,
+                    } = b;
+                    return (
+                        <NewsCard
+                            key={i}
+                            title={title}
+                            urlToImage={urlToImage}
+                            description={description}
+                            url={url}
+                            publishedAt={publishedAt}
+                            source={source}
+                            content={content}
+                        />
+                    );
+                })}
+        </section>
+    )
+}
 
-// export default SearchResult
+export default SearchResult
